f693b3e27730e0ddf2221c29c07067c1fa50cb90
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-09 12:15:53 +0800

    Create appendBook.java
    接口层
    添加图书

---
+++ jian/src/main/java/com/springboot/jian/controller/appendBook.java
@@ -0,0 +1,21 @@
+package com.springboot.jian.controller;
+
+import com.springboot.jian.service.appendBookMessage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class appendBook {
+//    添加图书
+    @GetMapping(value = "appendBook")
+    public int appendBook(
+            @RequestParam String name,
+            @RequestParam String author
+    ) throws IOException {
+        appendBookMessage appendBookMessage=new appendBookMessage();
+        return appendBookMessage.appendBook(name,author);
+    }
+}
