0077d7491a103cc82557507cc699173a8ae29e07
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-09 12:16:45 +0800

    Create borrowing.java
    接口层
    借阅图书记录

---
+++ jian/src/main/java/com/springboot/jian/controller/borrowing.java
@@ -0,0 +1,44 @@
+package com.springboot.jian.controller;
+
+import com.springboot.jian.service.BorrowingMessage;
+import com.springboot.jian.service.selectAllBookMessage;
+import com.springboot.jian.service.selectAllUserMessage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class borrowing {
+    //    添加借阅信息
+    @GetMapping(value = "borrowing")
+    public int borrowing(
+            @RequestParam String book,
+            @RequestParam String user
+    ) throws IOException {
+        int result = 0;
+//        这三个是三大金刚
+        BorrowingMessage borrowingMessage = new BorrowingMessage();
+        selectAllBookMessage selectAllBookMessage=new selectAllBookMessage();
+        selectAllUserMessage selectAllUserMessage=new selectAllUserMessage();
+
+//        判断图书是否存在
+        for (int i=0;i<selectAllBookMessage.selectAllBook().size();i++){
+            if(selectAllBookMessage.selectAllBook().get(i) ==book){
+//                判断用户是否存在
+                for (int j=0;j<selectAllUserMessage.selectAllUser().size();j++){
+                    if(selectAllUserMessage.selectAllUser().get(j)==user){
+                        result = borrowingMessage.Borrowing("book", "user");
+                    }else {
+                        result=-2;
+                    }
+                }
+            }else {
+                result=-1;
+            }
+        }
+        return result;
+    }
+
+}
