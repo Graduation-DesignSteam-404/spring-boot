390257a67592610da6a7d5fc35ec9bdf4ab3438c
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-09 12:15:11 +0800

    Create allUser.java
    接口层
    查询所有用户信息

---
+++ jian/src/main/java/com/springboot/jian/controller/allUser.java
@@ -0,0 +1,18 @@
+package com.springboot.jian.controller;
+
+import com.springboot.jian.service.allUserMessage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+public class allUser {
+//    查询所有用户信息
+    @GetMapping(value = "allUser")
+    public List<Object> allUser() throws IOException {
+        allUserMessage allUserMessage = new allUserMessage();
+        return allUserMessage.allUser();
+    }
+}
