1c78ba20a4825b925cc6caf13e6b6a30e54a6624
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-09 12:17:48 +0800

    Create returnBook.java
    接口层
    归还图书记录

---
+++ jian/src/main/java/com/springboot/jian/controller/returnBook.java
@@ -0,0 +1,41 @@
+package com.springboot.jian.controller;
+
+import com.springboot.jian.service.returnBookMessage;
+import com.springboot.jian.service.selectAllBookMessage;
+import com.springboot.jian.service.selectAllUserMessage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class returnBook {
+//    归还图书业务
+    @GetMapping(value = "returnBook")
+    public int returnBook(
+            @RequestParam String book,
+            @RequestParam String user
+    ) throws IOException {
+        int result=0;
+        returnBookMessage returnBookMessage=new returnBookMessage();
+        selectAllBookMessage selectAllBookMessage=new selectAllBookMessage();
+        selectAllUserMessage selectAllUserMessage=new selectAllUserMessage();
+        //        判断图书是否存在
+        for (int i=0;i<selectAllBookMessage.selectAllBook().size();i++){
+            if(selectAllBookMessage.selectAllBook().get(i) ==book){
+//                判断用户是否存在
+                for (int j=0;j<selectAllUserMessage.selectAllUser().size();j++){
+                    if(selectAllUserMessage.selectAllUser().get(j)==user){
+                        result=returnBookMessage.returnBook(book,user);
+                    }else {
+                        result=-2;
+                    }
+                }
+            }else {
+                result=-1;
+            }
+        }
+        return result;
+    }
+}
