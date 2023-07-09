2c5635896afd9fdcc5e2f08630f0bfe37fa50c4a
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-09 12:17:22 +0800

    Create removeUser.java
    接口层
    删除用户

---
+++ jian/src/main/java/com/springboot/jian/controller/removeUser.java
@@ -0,0 +1,20 @@
+package com.springboot.jian.controller;
+
+import com.springboot.jian.service.removeUserMessage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class removeUser {
+//    删除用户
+    @GetMapping(value = "removeUser")
+    public int removeUser(
+            @RequestParam String name
+    ) throws IOException {
+        removeUserMessage removeUserMessage=new removeUserMessage();
+        return removeUserMessage.removeUser(name);
+    }
+}
