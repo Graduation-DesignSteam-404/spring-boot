572764b46b97aa8110c81c4602d1c34b2caa5ec1
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-09 12:15:32 +0800

    Create allUserName.java
    接口层
    查询所有用户名

---
+++ jian/src/main/java/com/springboot/jian/controller/allUserName.java
@@ -0,0 +1,19 @@
+package com.springboot.jian.controller;
+
+import com.springboot.jian.service.selectAllUserMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@RestController
+public class allUserName {
+//    查询所有用户名
+    @GetMapping(value = "allUserName")
+    public ArrayList<Object> allUserName() throws IOException {
+        selectAllUserMessage selectAllUserMessage=new selectAllUserMessage();
+        return (ArrayList<Object>) selectAllUserMessage.selectAllUser();
+    }
+}
