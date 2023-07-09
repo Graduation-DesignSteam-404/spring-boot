9b77cec49d10d8a68fa4329aebf2c581ae50e8ce
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-09 12:19:56 +0800

    Update JianApplication.java
    @springBootApplication

--- jian/src/main/java/com/springboot/jian/JianApplication.java
+++ jian/src/main/java/com/springboot/jian/JianApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class JianApplication {
+public class
+JianApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JianApplication.class, args);
