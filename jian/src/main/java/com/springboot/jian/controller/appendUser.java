f14e3ef5dc49e8b1985d9f33ca56ef84fda55ad3
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-09 12:16:08 +0800

    Create appendUser.java
    接口层
    添加用户

---
+++ jian/src/main/java/com/springboot/jian/controller/appendUser.java
@@ -0,0 +1,20 @@
+package com.springboot.jian.controller;
+
+import com.springboot.jian.service.appendUserMessage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class appendUser {
+//    添加用户
+    @GetMapping(value = "appendUser")
+    public int appendUser(
+            @RequestParam String name
+    ) throws IOException {
+        appendUserMessage appendUserMessage=new appendUserMessage();
+        return appendUserMessage.appendUser(name);
+    }
+}
