7efb59a6fceb3a677a2ccc28074fb44c9be83d70
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-09 12:30:00 +0800

    Create selectAllUserMessage.java
    业务层
    查询所有用户名

---
+++ jian/src/main/java/com/springboot/jian/service/selectAllUserMessage.java
@@ -0,0 +1,28 @@
+package com.springboot.jian.service;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+//查询所有用户名
+public class selectAllUserMessage {
+    public List<Object> selectAllUser() throws IOException {
+        // 用于接收数据
+        List<Object> arrayList = new ArrayList<>();
+        // 创建 SqlSessionFactoryBuilder 对象
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        // 加载 MyBatis 配置文件
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        // 创建 SqlSessionFactory 对象
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 执行查询操作
+        arrayList= sqlSession.selectList("SelectUserName");
+        return arrayList;
+    }
+}
