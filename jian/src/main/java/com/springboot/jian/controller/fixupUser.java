01aadeca318c9c7fd989a636fa56a30f606e1270
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-09 12:16:57 +0800

    Create fixupUser.java
    接口层
    修改用户名

---
+++ jian/src/main/java/com/springboot/jian/controller/fixupUser.java
@@ -0,0 +1,21 @@
+package com.springboot.jian.controller;
+
+import com.springboot.jian.service.fixupUserMessage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class fixupUser {
+//    修改用户名
+    @GetMapping(value = "fixupUser")
+    public int fixupUser(
+            @RequestParam String newName,
+            @RequestParam String oldName
+    ) throws IOException {
+        fixupUserMessage fixupUserMessage=new fixupUserMessage();
+        return fixupUserMessage.fixUpUser(newName,oldName);
+    }
+}
