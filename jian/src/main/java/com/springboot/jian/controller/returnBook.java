39cdf7d36b8e628bba710507c131a0f739445bcb
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-14 19:27:12 +0800

    Update returnBook.java
    修复归还图书业务同样的问题

--- jian/src/main/java/com/springboot/jian/controller/returnBook.java
+++ jian/src/main/java/com/springboot/jian/controller/returnBook.java
@@ -17,23 +17,33 @@ public class returnBook {
             @RequestParam String user
     ) throws IOException {
         int result = 0;
+        boolean bookMessage = false;
+        boolean userMessage = false;
         returnBookMessage returnBookMessage = new returnBookMessage();
-        selectAllBookMessage selectAllBookMessage = new selectAllBookMessage();
         selectAllUserMessage selectAllUserMessage = new selectAllUserMessage();
         //        判断图书是否存在
-        for (int i = 0; i < selectAllBookMessage.selectAllBook().size(); i++) {
-            if (selectAllBookMessage.selectAllBook().get(i) == book) {
-//                判断用户是否存在
-                for (int j = 0; j < selectAllUserMessage.selectAllUser().size(); j++) {
-                    if (selectAllUserMessage.selectAllUser().get(j) == user) {
-                        result = returnBookMessage.returnBook(book, user);
-                    } else {
-                        result = -2;
-                    }
-                }
-            } else {
-                result = -1;
+        for (int i = 0; i < com.springboot.jian.service.selectAllBookMessage.selectAllBook().size(); i++) {
+            if (com.springboot.jian.service.selectAllBookMessage.selectAllBook().get(i).equals(book)) {
+                bookMessage = true;
+                break;
             }
+        }
+//        判断用户是否存在
+        for (int j = 0; j < selectAllUserMessage.selectAllUser().size(); j++) {
+            if (selectAllUserMessage.selectAllUser().get(j).equals(user)) {
+                result = returnBookMessage.returnBook(book, user);
+                userMessage = true;
+                break;
+            }
+        }
+        if (!bookMessage) {
+            result = -1;
+        }
+        if (!userMessage) {
+            result = -2;
+        }
+        if (bookMessage && userMessage) {
+            result = 1;
         }
         return result;
     }
